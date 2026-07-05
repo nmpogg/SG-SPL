9b27694c4374e8f0387e8744c91ffd9591e32ae7
Akimboo <149180748+nmpogg@users.noreply.github.com>
2026-07-05 16:12:32 +0700

    feat: add prompt to VisionTransformer and CLIP encoding methods

--- clip/model.py
+++ clip/model.py
@@ -220,12 +220,15 @@ class VisionTransformer(nn.Module):
         self.ln_post = LayerNorm(width)
         self.proj = nn.Parameter(scale * torch.randn(width, output_dim))
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor, prompt: torch.Tensor = None):
         x = self.conv1(x)  # shape = [*, width, grid, grid]
         x = x.reshape(x.shape[0], x.shape[1], -1)  # shape = [*, width, grid ** 2]
         x = x.permute(0, 2, 1)  # shape = [*, grid ** 2, width]
         x = torch.cat([self.class_embedding.to(x.dtype) + torch.zeros(x.shape[0], 1, x.shape[-1], dtype=x.dtype, device=x.device), x], dim=1)  # shape = [*, grid ** 2 + 1, width]
         x = x + self.positional_embedding.to(x.dtype)
+        if prompt is not None:
+            # prompt should be of shape [*, N, width]
+            x = torch.cat([x, prompt], dim=1) # [*, grid ** 2 + 1 + N, width]
         x = self.ln_pre(x)
 
         x = x.permute(1, 0, 2)  # NLD -> LND
@@ -337,8 +340,11 @@ class CLIP(nn.Module):
     def dtype(self):
         return self.visual.conv1.weight.dtype
 
-    def encode_image(self, image):
-        return self.visual(image.type(self.dtype))
+    def encode_image(self, image, prompt=None):
+        if prompt is not None:
+            return self.visual(image.type(self.dtype), prompt.type(self.dtype))
+        else:
+            return self.visual(image.type(self.dtype))
 
     def encode_text(self, text):
         x = self.token_embedding(text).type(self.dtype)  # [batch_size, n_ctx, d_model]
