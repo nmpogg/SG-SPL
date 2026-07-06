c1d15e960a708e3805def511f29b91d23c9e5d8b
Akimboo <149180748+nmpogg@users.noreply.github.com>
2026-07-06 22:12:13 +0700

    fix: update training progress bar

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ class CustomProgressBar(TQDMProgressBar):
 
     def init_train_tqdm(self):
         bar = super().init_train_tqdm()
-        bar.leave = False
+        bar.leave = True
         return bar
 
     def init_validation_tqdm(self):
@@ -16,9 +16,3 @@ class CustomProgressBar(TQDMProgressBar):
             leave=False,
             dynamic_ncols=True,
         )
-
-    def init_sanity_tqdm(self):
-        bar = super().init_sanity_tqdm()
-        bar.set_description("Sanity Check")
-        bar.leave = False
-        return bar
